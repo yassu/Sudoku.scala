d35fd21e9228ccfc404a20f7f5ed150bacf3bd59
yassu <yassu0320.dev@gmail.com>
2018-06-23 01:18:02 +0900

    funcProduct or funcProducts to A parameter

--- src/main/scala/FuncUtil.scala
+++ src/main/scala/FuncUtil.scala
@@ -1,25 +1,25 @@
 package sudoku
 
 object FuncUtil {
-  def funcProduct(fs: (Int => Int, Int)): List[Int => Int] =
+  def funcProduct[A](fs: (A => A, Int)): List[A => A] =
     if (fs._2 == 0) {
-      val f: Int => Int = x => x
+      val f: A => A = x => x
       List(f)
     }
     else {
       val beforeFuncs = funcProduct(fs._1, fs._2 - 1)
       funcProduct((fs._1, fs._2 - 1)) ++ List(
-        (x: Int) => fs._1(beforeFuncs.last(x))
+        (x: A) => fs._1(beforeFuncs.last(x))
       )
     }
 
-  def funcProducts(fs: List[(Int => Int, Int)]): List[Int => Int] =
+  def funcProducts[A](fs: List[(A => A, Int)]): List[A => A] =
     if (fs.isEmpty) List()
     else {
       val beforeFuncs = funcProducts(fs.drop(1))
       val nowFuncs = funcProduct(fs.head)
       funcProducts(fs.drop(1)) ++ funcProduct(fs.head) ++
-        (for (fs <- beforeFuncs; gs <- nowFuncs) yield((x: Int) => gs(fs(x))))
+        (for (fs <- beforeFuncs; gs <- nowFuncs) yield((x: A) => gs(fs(x))))
     }
 }
 
