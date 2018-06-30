36f838364eaba901d98b7837f9dd8bbb8cb3cf96
yassu <yassu0320.dev@gmail.com>
2018-06-30 16:12:40 +0900

    SudokuXRules

---
+++ src/main/scala/UniqueLineRules.scala
@@ -0,0 +1,41 @@
+package sudoku
+
+sealed abstract class UniqueLineRules {
+  def uniquePositions: Seq[(Int, Int)]
+}
+
+object UniqueLineRules {
+  def sudokuXRules: Seq[UniqueLineRules] =
+    (0 until 9).map(new OneColumnRule(_)) ++
+    (0 until 9).map(new OneRowRule(_)) ++
+    (for (yCell <- (0 until 3); xCell <- (0 until 3)) yield new OneCellRule(xCell, yCell)) :+
+    (new DiagonalLineRule) :+
+    (new InvDiagonalLineRule)
+}
+
+private[sudoku] class OneColumnRule(colNumber: Int) extends UniqueLineRules {
+  val uniquePositions: Seq[(Int, Int)] =
+    for (y <- (0 until 9)) yield (colNumber, y)
+}
+
+private[sudoku] class OneRowRule(rowNumber: Int) extends UniqueLineRules {
+  val uniquePositions: Seq[(Int, Int)] =
+    for (x <- (0 until 9)) yield (x, rowNumber)
+}
+
+private[sudoku] class OneCellRule(xCellNumber: Int, yCellNumber: Int) extends UniqueLineRules {
+  val uniquePositions: Seq[(Int, Int)] =
+    for (
+      y <- (3 * yCellNumber until 3 * yCellNumber + 3);
+      x <- (3 * xCellNumber until 3 * xCellNumber + 3)) yield (x, y)
+}
+
+private[sudoku] class DiagonalLineRule extends UniqueLineRules {
+  val uniquePositions: Seq[(Int, Int)] =
+    for (j <- (0 until 9)) yield (j, j)
+}
+
+private[sudoku] class InvDiagonalLineRule extends UniqueLineRules {
+  val uniquePositions: Seq[(Int, Int)] =
+    for (j <- (0 until 9)) yield (8 - j, j)
+}
---
+++ src/test/scala/UniqueLineRulesSpec.scala
@@ -0,0 +1,66 @@
+package sudoku
+
+import org.scalatest.FunSpec
+
+class OneColumnRuleSpec extends FunSpec {
+  it ("uniquePositions") {
+    val rule = new OneColumnRule(2)
+    assert(rule.uniquePositions == Seq(
+      (2, 0), (2, 1), (2, 2), (2, 3), (2, 4), (2, 5), (2, 6), (2, 7), (2, 8)
+    ))
+  }
+}
+
+class OneRowRuleSpec extends FunSpec {
+  it ("uniquePositions") {
+    val rule = new OneRowRule(2)
+    assert(rule.uniquePositions == Seq(
+      (0, 2), (1, 2), (2, 2), (3, 2), (4, 2), (5, 2), (6, 2), (7, 2), (8, 2)
+    ))
+  }
+}
+
+class OneCellRuleSpec extends FunSpec {
+  it ("uniquePositions") {
+    val rule = new OneCellRule(1, 2)
+    assert(rule.uniquePositions == Seq(
+      (3, 6), (4, 6), (5, 6),
+      (3, 7), (4, 7), (5, 7),
+      (3, 8), (4, 8), (5, 8)
+    ))
+  }
+}
+
+class DiagonalLineRuleSpec extends FunSpec {
+  it ("uniquePositions") {
+    val rule = new DiagonalLineRule
+    assert(rule.uniquePositions == Seq(
+      (0, 0),
+      (1, 1),
+      (2, 2),
+      (3, 3),
+      (4, 4),
+      (5, 5),
+      (6, 6),
+      (7, 7),
+      (8, 8)
+    ))
+  }
+}
+
+class InvDiagonalLineRuleSpec extends FunSpec {
+  it ("uniquePositions") {
+    val rule = new InvDiagonalLineRule
+    assert(rule.uniquePositions == Seq(
+      (8, 0),
+      (7, 1),
+      (6, 2),
+      (5, 3),
+      (4, 4),
+      (3, 5),
+      (2, 6),
+      (1, 7),
+      (0, 8),
+    ))
+  }
+}
